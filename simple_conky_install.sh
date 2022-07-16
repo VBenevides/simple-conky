6b6da34e5e9af3e124b5626ba93c0b359b88a6c4
Vinicius Benevides <massaki1999@gmail.com>
2022-07-16 11:38:49 +0000

    remove .git from target

--- simple_conky_install.sh
+++ simple_conky_install.sh
@@ -4,5 +4,6 @@ BASEDIR=$(dirname "$0")
 echo "$BASEDIR"
 
 # Move Simple Conky Directory to .conky
-#mkdir -p $HOME/.conky
-cp -R "$BASEDIR/Simple Conky" "$BASEDIR/Simple Conky"
+mkdir -p $HOME/.conky
+cp -r "$BASEDIR/../Simple Conky" "$HOME/.conky/Simple Conky"
+rm -rf "$HOME/.conky/Simple Conky/.git"
